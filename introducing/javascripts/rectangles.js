23f79759867ae667850ee050123b1324b221d3c2
Ram Iyer <ramky.iyer@yahoo.com>
2014-04-10 12:20:39 -0700

    completed introduction

---
+++ introducing/javascripts/rectangles.js
@@ -0,0 +1,50 @@
+// Don't use the global namespace
+(function() {
+  var Rectangle     = Backbone.Model.extend({ });
+  var RectangleView = Backbone.View.extend({ 
+    tagName: 'div',
+    className: 'rectangle',
+    events: {
+      'click': 'move'
+    },
+    render: function() {
+      this.setDimensions();
+      this.setPosition();
+      this.setColor();
+     return this;
+    },
+    setDimensions: function() {
+      this.$el.css({
+	width: this.model.get('width') + 'px',
+	height: this.model.get('height') + 'px'
+       });
+    },
+    setPosition: function() {
+      var position = this.model.get('position');
+      this.$el.css({
+	left: position.x,
+	top: position.y
+      });
+    },
+    setColor: function() {
+      this.$el.css('background-color', this.model.get('color'));
+    },
+    move: function() {
+      this.$el.css('left', this.$el.position().left + 10); 
+    }
+  });
+
+  var myRectangle = new Rectangle({
+    width: 100,
+    height: 60,
+    position: {
+      x: 300,
+      y: 150
+    },
+    color: '#ff0000'
+  });
+
+  var myView = new RectangleView({ model: myRectangle });
+  $('div#canvas').append(myView.render().el);
+
+})();
